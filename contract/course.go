d0547476db6132927592c98dc7ec9cec55809baf
Herbert Abdillah <herbert.abdillah@gmail.com>
2023-06-27 15:15:43 +0700

    refactor course 2

--- contract/course.go
+++ contract/course.go
@@ -25,111 +25,39 @@ func InsertCoursePlan(c router.Context) (interface{}, error) {
 		return nil, err
 	}
 
-	result := []state.CourseSemeterResult{}
-
-	for _, courseSemesterId := range courseSemesterIds {
-		courseSemester, err := cc.Repository.GetCourseSemester(courseSemesterId)
-		if err != nil {
-			return nil, err
-		}
-		csr := state.CourseSemeterResult{CourseSemesterId: courseSemesterId, Score: 0, Pass: false, CourseId: courseSemester.CourseId}
-		result = append(result, csr)
+	_, err = generateCourseResult(cc, coursePlan)
+	if err != nil {
+		return nil, err
 	}
-
-	courseResult := &state.CourseResult{Id: id, Year: year, Semester: semester, StudentId: studentId, CoursePlanId: id, Result: result}
-	_, err = cc.Repository.InsertCourseResult(courseResult)
 
 	return coursePlan, err
 }
 
 func UpdateCourseResult(c router.Context) (interface{}, error) {
 	cc := NewContext(c)
-	var err error
 	courseSemesterId, courseResultId, scoreStr := c.ParamString("courseSemesterId"), c.ParamString("courseResultId"), c.ParamString("score")
 	score, _ := strconv.ParseFloat(scoreStr, 64)
+
 	courseResult, err := cc.Repository.GetCourseResult(courseResultId)
 	if err != nil {
 		return nil, err
 	}
-	totalScore := 0.0
-	totalCredit := 0
-	isPass := false
-	var currentCourseId string
-	var currentCredit int
-	for i, courseSemesterResult := range courseResult.Result {
-		if courseSemesterResult.CourseSemesterId == courseSemesterId {
-			currentCourseId = courseSemesterResult.CourseId
-			courseSemesterResult.Score = score
-			if score >= 2 {
-				courseSemesterResult.Pass = true
-				isPass = true
-			} else {
-				courseSemesterResult.Pass = false
-			}
-			courseResult.Result[i] = courseSemesterResult
-		}
-		courseSemester, err := cc.Repository.GetCourseSemester(courseSemesterResult.CourseSemesterId)
-		if err != nil {
-			return nil, err
-		}
 
-		course, err := cc.Repository.GetCourse(courseSemester.CourseId)
-		if err != nil {
-			return nil, err
-		}
-
-		if courseSemesterResult.CourseSemesterId == courseSemesterId {
-			currentCredit = course.Credit
-		}
-		totalScore += courseSemesterResult.Score * float64(course.Credit)
-		totalCredit += course.Credit
+	if err := cc.Service.Updatable(courseResult.Year, courseResult.Semester); err != nil {
+		return nil, err
 	}
-	resultScore := totalScore / float64(totalCredit)
-	courseResult.Score = resultScore
 
-	transcript, err := cc.Repository.GetTranscript(courseResult.StudentId)
+	isPass := false
+	if score >= 2 {
+		isPass = true
+	}
+
+	_, course, err := updateCourseResult(cc, courseResult, courseSemesterId, score, isPass)
 	if err != nil {
 		return nil, err
 	}
 
-	found := false
-	totalTranscriptScore := 0.0
-	totalTranscriptCredit := 0
-	foundIndex := -1
-	var foundResult state.TranscriptResult
-	for i, transcriptResult := range transcript.TranscriptResult {
-		if transcriptResult.CourseId == currentCourseId {
-			found = true
-			foundIndex = i
-			foundResult = transcriptResult
-			foundResult.Score = score
-			foundResult.Pass = isPass
-			foundResult.CourseResultId = courseResultId
-			foundResult.Year = courseResult.Year
-			foundResult.Semester = courseResult.Semester
-		} else {
-			course, err := cc.Repository.GetCourse(transcriptResult.CourseId)
-			if err != nil {
-				return nil, err
-			}
-
-			totalTranscriptCredit += course.Credit
-			totalTranscriptScore += (float64(course.Credit) * transcriptResult.Score)
-		}
-	}
-
-	if found {
-		transcript.TranscriptResult[foundIndex] = foundResult
-	} else {
-		newTranscriptResult := state.TranscriptResult{CourseResultId: courseResultId, CourseId: currentCourseId, Year: courseResult.Year, Semester: courseResult.Semester, Score: score, Pass: isPass}
-		transcript.TranscriptResult = append(transcript.TranscriptResult, newTranscriptResult)
-	}
-	totalTranscriptCredit += currentCredit
-	totalTranscriptScore += (float64(currentCredit) * score)
-
-	transcript.Score = totalTranscriptScore / float64(totalTranscriptCredit)
-
-	_, err = cc.Repository.UpdateTranscript(transcript)
+	_, err = updateTranscript(cc, courseResult, course.Id, score, isPass, course.Credit)
 	if err != nil {
 		return nil, err
 	}
@@ -149,4 +77,114 @@ func GetCourseResult(c router.Context) (interface{}, error) {
 	id := c.ParamString("id")
 
 	return cc.Repository.GetCourseResult(id)
+}
+
+func generateCourseResult(cc Context, cp *state.CoursePlan) (*state.CourseResult, error) {
+	result := []state.CourseSemeterResult{}
+
+	for _, courseSemesterId := range cp.CourseSemesterIds {
+		courseSemester, err := cc.Repository.GetCourseSemester(courseSemesterId)
+		if err != nil {
+			return nil, err
+		}
+		csr := state.CourseSemeterResult{CourseSemesterId: courseSemesterId, Score: 0, Pass: false, CourseId: courseSemester.CourseId}
+		result = append(result, csr)
+	}
+
+	courseResult := &state.CourseResult{Id: cp.Id, Year: cp.Year, Semester: cp.Semester, StudentId: cp.StudentId, CoursePlanId: cp.Id, Result: result}
+	_, err := cc.Repository.InsertCourseResult(courseResult)
+	if err != nil {
+		return nil, err
+	}
+	return courseResult, nil
+}
+
+func updateCourseResult(cc Context, cr *state.CourseResult, courseSemesterId string, score float64, isPass bool) (*state.CourseResult, *state.Course, error) {
+	var currentCourse *state.Course
+	totalScore := 0.0
+	totalCredit := 0
+
+	for i, courseSemesterResult := range cr.Result {
+		courseSemester, err := cc.Repository.GetCourseSemester(courseSemesterResult.CourseSemesterId)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		course, err := cc.Repository.GetCourse(courseSemester.CourseId)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		if courseSemesterResult.CourseSemesterId == courseSemesterId {
+			currentCourse = course
+
+			courseSemesterResult.Score = score
+			courseSemesterResult.Pass = isPass
+			cr.Result[i] = courseSemesterResult
+		}
+		totalScore += courseSemesterResult.Score * float64(course.Credit)
+		totalCredit += course.Credit
+	}
+
+	resultScore := totalScore / float64(totalCredit)
+	cr.Score = resultScore
+
+	_, err := cc.Repository.UpdateCourseResult(cr)
+
+	if err != nil {
+		return nil, currentCourse, err
+	}
+
+	return cr, currentCourse, nil
+}
+
+func updateTranscript(cc Context, cr *state.CourseResult, currentCourseId string, score float64, isPass bool, credit int) (*state.Transcript, error) {
+	transcript, err := cc.Repository.GetTranscript(cr.StudentId)
+	if err != nil {
+		return nil, err
+	}
+
+	found := false
+	totalTranscriptScore := 0.0
+	totalTranscriptCredit := 0
+	foundIndex := -1
+	var foundResult state.TranscriptResult
+	for i, transcriptResult := range transcript.TranscriptResult {
+		if transcriptResult.CourseId == currentCourseId {
+			found = true
+			foundIndex = i
+			foundResult = transcriptResult
+			foundResult.Score = score
+			foundResult.Pass = isPass
+			foundResult.CourseResultId = cr.Id
+			foundResult.Year = cr.Year
+			foundResult.Semester = cr.Semester
+		} else {
+			course, err := cc.Repository.GetCourse(transcriptResult.CourseId)
+			if err != nil {
+				return nil, err
+			}
+
+			totalTranscriptCredit += course.Credit
+			totalTranscriptScore += (float64(course.Credit) * transcriptResult.Score)
+		}
+	}
+
+	if found {
+		transcript.TranscriptResult[foundIndex] = foundResult
+	} else {
+		newTranscriptResult := state.TranscriptResult{CourseResultId: cr.Id, CourseId: currentCourseId, Year: cr.Year, Semester: cr.Semester, Score: score, Pass: isPass}
+		transcript.TranscriptResult = append(transcript.TranscriptResult, newTranscriptResult)
+	}
+	totalTranscriptCredit += credit
+	totalTranscriptScore += (float64(credit) * score)
+
+	transcript.Score = totalTranscriptScore / float64(totalTranscriptCredit)
+
+	_, err = cc.Repository.UpdateTranscript(transcript)
+	if err != nil {
+		return nil, err
+	}
+
+	return transcript, nil
 }
